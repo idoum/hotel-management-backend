73f8b0dc1efb07a4da1358cdf55f0e33f0f68b06
Issa Doumbia <doumbia.issa@gmail.com>
2025-08-15 10:50:53 -0400

    mise a jour de validate.js

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,14 +1,23 @@
-const Joi = require('joi');
+// src/middlewares/validate.js
 
-const validateBody = (schema) => (req, res, next) => {
-  const { error } = schema.validate(req.body, { abortEarly: false });
-  if (error) {
-    return res.status(400).json({
-      message: "Validation échouée",
-      details: error.details.map(d => d.message)
-    });
-  }
-  next();
+/**
+ * Middleware générique de validation pour Express utilisant Joi.
+ * @param {Joi.Schema} schema - Le schéma de validation Joi à utiliser.
+ * @param {string} [property='body'] - Partie de la requête à valider ('body', 'params', 'query').
+ * @returns {Function} Middleware Express pour valider la requête.
+ */
+const validate = (schema, property = 'body') => {
+  return (req, res, next) => {
+    // Utilisation de abortEarly:false pour avoir toutes les erreurs en une fois
+    const { error } = schema.validate(req[property], { abortEarly: false });
+    if (error) {
+      return res.status(400).json({
+        message: 'Validation échouée',
+        details: error.details.map((d) => d.message)
+      });
+    }
+    next();
+  };
 };
 
-module.exports = { validateBody };
+module.exports = { validate };
